a292622d002591ae03cd692ef74fb9fe991d8716
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-06-24 18:52:44 +0300

    Edited product

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,7 @@ import Application from "@/components/Pages/Application/Application";
 import Benefits from "@/components/Pages/Benefits/Benefits";
 import Product from "@/components/Pages/Product/Product";
 import { getProduct } from "@/utils/getProduct";
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { FC } from "react";
 
@@ -9,6 +10,24 @@ interface PageProps {
   params: {
     id: string;
   };
+}
+
+export async function generateMetadata({ params }: PageProps): Promise<Metadata> {
+  const product = await getProduct(params.id);
+
+  if (!product) {
+    const metadata: Metadata = {
+      title: "Ошибка",
+    };
+
+    return metadata;
+  }
+
+  const metadata: Metadata = {
+    title: product.title,
+  };
+
+  return metadata;
 }
 
 const Page: FC<PageProps> = async ({ params }) => {
